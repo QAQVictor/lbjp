3577b7ae7d3efe064c7dacf5621555050c4c818c
liyaqing <liyaqing94@gmail.com>
2018-04-30 23:26:11 +0800

    添加 index.html

--- src/main/java/com/HelloSpringBoot.java
+++ src/main/java/com/HelloSpringBoot.java
@@ -1,14 +1,11 @@
 package com;
 
-import com.user.model.DO.UserDO;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import javax.servlet.http.HttpSession;
 import java.util.HashMap;
 
 /**
@@ -25,15 +22,19 @@ public class HelloSpringBoot {
         SpringApplication.run(HelloSpringBoot.class, args);
     }
 
-    @RequestMapping("/hello")
-    public String index(HashMap<String, Object> map) {
+    @RequestMapping("/index")
+    public String index() {
         return "/index";
     }
 
     @RequestMapping("/registerPage")
-    public String loginPage() {
+    public String registerPage() {
         return "/registerPage";
     }
 
+    @RequestMapping("/loginPage")
+    public String loginPage() {
+        return "/loginPage";
+    }
 
 }
